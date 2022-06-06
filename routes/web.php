01dc857e0504bd600b8dc63bfa69f0fc7a1c32f4
sonnx <sonnx@vietis.com.vn>
2022-06-06 17:38:42 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/name/{name}/{age}', function ($naame, $tuoi){
+    return $naame.'---'.$tuoi;
+});
